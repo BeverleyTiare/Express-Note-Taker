3271a16753d0ab840f0849820b1fc0f814af3130
BeverleyTiare <122957548+BeverleyTiare@users.noreply.github.com>
2023-04-28 18:48:26 +1000

    change port

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/api/notes', (req, res) => {
   }
 })
 
-const PORT = process.env.port || 5001
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
